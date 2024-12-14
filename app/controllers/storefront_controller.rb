5abce2048b8a1e987d2f5cf0a493bf7097abe86e
rami <ramialvarez85@gmail.com>
2024-12-13 23:58:37 -0300

    Ultimo cambio

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -2,7 +2,7 @@ class StorefrontController < ApplicationController
     
     def index
         # Iniciar con todos los productos
-        @products = Product.page(params[:page]).per(6)
+        @products = Product.where(deleted_at: nil).page(params[:page]).per(6)
     
         # Filtro por categorías (múltiples)
         if params[:category_ids].present?
